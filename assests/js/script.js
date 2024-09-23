df57cb23d33b77009b5f3fd8059279d64b405219
unknown <bhartikhushi32@gmail.com>
2024-09-23 17:06:42 +0530

    1st commit for cravefx website

---
+++ assests/js/script.js
@@ -0,0 +1,53 @@
+const nav1 = document.querySelector('.navbar');
+
+window.addEventListener('scroll', () => {
+  if (window.scrollY >= 100) {
+    nav1.classList.add('scroll-down')
+  }
+  else if (window.scrollY <= 100) {
+    nav1.classList.remove('scroll-down')
+  }
+})
+
+
+AOS.init();
+
+new Swiper('.slider-wrapper', {
+
+  loop: true,
+  gapCursor: true,
+  spaceBetween: 20,
+  autoplay: {
+    delay: 1000,
+    disableOnInteraction: false
+  },
+  // If we need pagination
+  pagination: {
+    el: '.swiper-pagination',
+    clickable: true,
+    dynamicBultets: true
+  },
+
+  // Navigation arrows
+  navigation: {
+    nextEl: '.swiper-button-next',
+    prevEl: '.swiper-button-prev',
+  },
+
+  breakpoints: {
+    0: {
+      slidesPerView: 2
+    },
+    576: {
+      slidesPerView: 3
+    },
+    768: {
+      slidesPerView: 4
+    },
+    1200: {
+      slidesPerView: 6
+    }
+
+  }
+
+});
